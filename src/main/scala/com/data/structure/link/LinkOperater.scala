980f36d424b612b5a96ad65d42a8529f1eb7f2da
LinMQ <Master@LMQ-PC>
2018-09-08 15:11:58 +0800

    约瑟夫

--- src/main/scala/com/data/structure/link/LinkOperater.scala
+++ src/main/scala/com/data/structure/link/LinkOperater.scala
@@ -11,46 +11,57 @@ object LinkOperater {
     val d = SingleNode(4, e)
     val c = SingleNode(3, d)
     val b = SingleNode(2, c)
-    val a = SingleNode(1, b) //head
+    val head = SingleNode(1, b) //head
 
+    //reverseLink(a).print()
     
-    reverseLink(a).print()
+    JosephosUnRecursion(head, 3)
+    head.print()
   }
   /**
    * 反转单链表
    */
-  def reverseLink(tmp: SingleNode)= {
-    var head: SingleNode=tmp
+  def reverseLink(tmp: SingleNode) = {
+    var head: SingleNode = tmp
     var nextHead: SingleNode = null
     var pre: SingleNode = null
     while (head != null) {
-      
+
       //以下三步为反转操作。就是next和pre的互换。把当前head的next变成上一个pre。（注意要先把head.next拿出来）
-    	nextHead = head.next //先把下一个记下来
-      head.next=pre//将上一个 赋值给当前node的next。
+      nextHead = head.next //先把下一个记下来
+      head.next = pre //将上一个 赋值给当前node的next。
       pre = head //记录下 ： 上一个
-            
-      head=nextHead  //开始计算下一个
+
+      head = nextHead //开始计算下一个
     }
-   pre
+    pre
   }
-}
-case class SingleNode(
-    var value: Int,
-    var next: SingleNode) {
-  def print() {
-    println(value)
-    if (next != null)
-      next.print
+  /**
+   * 约瑟夫环问题 (非递归方法)
+   * m:数到第几就kill
+   */
+  def JosephosUnRecursion(head: SingleNode, m: Int) {
+    var pre: SingleNode = head
+    var current: SingleNode = head.next
+    var num = 1 //from 1
+    while (current != null) {
+      num+=1
+      if (num == m) {//kill
+        pre.next=current.next//delete current node
+        num=0
+      }else {
+        pre=current
+      }
+      current=current.next
+    }
+
   }
-}
-case class DoubleNode(
-    var velue: Int,
-    var per: DoubleNode,
-    var next: DoubleNode) {
-  def print() {
-    if (next != null)
-      next.print
-    println(velue)
+  /**
+   * 约瑟夫环问题 (递归方法)
+   * m:数到第几就kill
+   * live:剩下多少人停止
+   */
+  def JosephosRecursion(head: SingleNode, m: Int, live: Int) {
+
   }
 }

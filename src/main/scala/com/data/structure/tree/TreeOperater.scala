bc7196391a6a79f138a1ff05137f694551b617aa
LMQ <LMQ@PV-X00255651>
2019-02-22 11:35:30 +0800

    前，中，后 序遍历 （递归）

--- src/main/scala/com/data/structure/tree/TreeOperater.scala
+++ src/main/scala/com/data/structure/tree/TreeOperater.scala
@@ -1,5 +1,61 @@
 package com.data.structure.tree
 
+import scala.collection.mutable.ArrayBuffer
+import java.util.ArrayList
+
 object TreeOperater {
-  
+  /*
+   * 											4
+   * 										/   \
+   * 									 2		 6
+   * 								  / \   / \
+   * 								 1   3 5   7
+   */
+  def main(args: Array[String]): Unit = {
+    val node_1 = new TreeNode(1, null, null)
+    val node_3 = new TreeNode(3, null, null)
+    val node_2 = new TreeNode(2, node_1, node_3)
+    val node_5 = new TreeNode(5, null, null)
+    val node_7 = new TreeNode(7, null, null)
+    val node_6 = new TreeNode(6, node_5, node_7)
+    val head = new TreeNode(4, node_2, node_6)
+    //(middleTraversalTree(head))
+    //(leftTraversalTree(head))
+    //(rightTraversalTree(head))
+  }
+
+  def middleTraversalTree(head: TreeNode) {
+    if (head == null) return
+    middleTraversalTree(head.left)
+    println(head.value)
+    middleTraversalTree(head.right)
+  }
+  def leftTraversalTree(head: TreeNode) {
+    if (head == null) return
+    println(head.value)
+    leftTraversalTree(head.left)
+    leftTraversalTree(head.right)
+  }
+  def rightTraversalTree(head: TreeNode) {
+    if (head == null) return
+    rightTraversalTree(head.left)
+    rightTraversalTree(head.right)
+    println(head.value)
+  }
+}
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = null
+  var right: TreeNode = null
+  def this(value: Int, left: TreeNode, right: TreeNode) {
+    this(value)
+    this.left = left
+    this.right = right
+  }
+  def setLeft(left: TreeNode) {
+    this.left = left
+  }
+  def setRight(right: TreeNode) {
+    this.right = right
+  }
 }
